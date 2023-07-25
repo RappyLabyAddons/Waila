1eea0b564339ca54719169c19113db9e2c91202d
RappyTV <rappytv@rappytv.com>
2023-07-26 00:49:01 +0200

    Remove 1.8.9 from supported minecraft versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ labyMod {
         displayName = "Waila"
         author = "RappyTV"
         description = "Adds a Widget, which displays what block you're looking at."
-        minecraftVersion = "*"
+        minecraftVersion = "1.12<1.20.1"
         version = System.getenv().getOrDefault("VERSION", "0.0.9")
     }
 
