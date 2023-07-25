ebfd49727abc0cd16e142130b437db2621326353
RappyTV <rappytv@rappytv.com>
2023-07-25 22:07:55 +0200

    Add 1.19 implementations

---
+++ game-runner/src/v1_19_2/java/com/rappytv/waila/v1_19_2/ImplWailaApi.java
@@ -0,0 +1,28 @@
+package com.rappytv.waila.v1_19_2;
+
+import com.rappytv.waila.util.IWailaApi;
+import net.labymod.api.models.Implements;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.HitResult.Type;
+
+@Implements(IWailaApi.class)
+public class ImplWailaApi implements IWailaApi {
+
+    @Override
+    public String getLookingAt(boolean fluid, int range) {
+        Minecraft minecraft = Minecraft.getInstance();
+        LocalPlayer player = minecraft.player;
+
+        if(player == null) return null;
+        HitResult result = player.pick(range, 0f, fluid);
+        if(result.getType() != Type.BLOCK || minecraft.level == null) return null;
+
+        BlockState state = minecraft.level.getBlockState(((BlockHitResult) result).getBlockPos());
+
+        return state.getBlock().getName().getString();
+    }
+}
