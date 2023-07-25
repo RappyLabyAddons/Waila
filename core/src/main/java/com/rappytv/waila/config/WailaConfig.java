77e3baaed0b6a9dbc796d1a89e07997e0628c455
RappyTV <rappytv@rappytv.com>
2023-07-26 01:49:58 +0200

    Add config icons

--- core/src/main/java/com/rappytv/waila/config/WailaConfig.java
+++ core/src/main/java/com/rappytv/waila/config/WailaConfig.java
@@ -4,14 +4,19 @@ import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SliderWidget.SliderSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.annotation.SpriteSlot;
+import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 @ConfigName("settings")
+@SpriteTexture("settings")
 public class WailaConfig extends AddonConfig {
 
     @SwitchSetting
+    @SpriteSlot(size = 32)
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
     @SliderSetting(min = 5, max = 20)
+    @SpriteSlot(size = 32, x = 1)
     private final ConfigProperty<Integer> range = new ConfigProperty<>(5);
 
     @Override
