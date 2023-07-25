6a548cd537b63c9edd69e85e2339a0766552ff3c
RappyTV <rappytv@rappytv.com>
2023-07-25 20:19:47 +0200

    Add range slider config option

--- core/src/main/java/com/rappytv/waila/config/WailaConfig.java
+++ core/src/main/java/com/rappytv/waila/config/WailaConfig.java
@@ -1,6 +1,7 @@
 package com.rappytv.waila.config;
 
 import net.labymod.api.addon.AddonConfig;
+import net.labymod.api.client.gui.screen.widget.widgets.input.SliderWidget.SliderSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
@@ -10,9 +11,14 @@ public class WailaConfig extends AddonConfig {
 
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
+    @SliderSetting(min = 5, max = 20)
+    private final ConfigProperty<Integer> range = new ConfigProperty<>(5);
 
     @Override
     public ConfigProperty<Boolean> enabled() {
         return enabled;
+    }
+    public ConfigProperty<Integer> range() {
+        return range;
     }
 }
