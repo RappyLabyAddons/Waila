c0c0fb42e978afe4e3a79ff4a9aad20a799be1fd
RappyTV <rappytv@rappytv.com>
2024-07-03 12:55:17 +0200

    Make IWailaApi#getLookingAt nullable

--- core/src/main/java/com/rappytv/waila/util/IWailaApi.java
+++ core/src/main/java/com/rappytv/waila/util/IWailaApi.java
@@ -1,8 +1,11 @@
 package com.rappytv.waila.util;
 
 import net.labymod.api.reference.annotation.Referenceable;
+import org.jetbrains.annotations.Nullable;
 
 @Referenceable
 public interface IWailaApi {
+
+    @Nullable
     String getLookingAt(boolean fluid, int range);
 }
